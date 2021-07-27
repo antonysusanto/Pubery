1f844899f971df598c2b80b587af2e52b2e0bbdc
Carlos Gamaliel Manurung <carlosgamaliel>
2021-07-27 16:45:45 +0700

    Create entity and attribute for core data

---
+++ Pubery/Utilities/Core Data/Managed Object/Progress+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Progress+CoreDataClass.swift
+//  Pubery
+//
+//  Created by Carlos Gamaliel Manurung on 27/07/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Progress)
+public class Progress: NSManagedObject {
+
+}
