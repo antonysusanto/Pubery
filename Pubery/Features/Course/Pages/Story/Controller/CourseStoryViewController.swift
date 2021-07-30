3d2955d868c5270309bd65363856ed0e2675c3f7
antony-hc <antony@hepicircle.com>
2021-07-31 03:27:42 +0700

    v0.1

--- Pubery/Features/Course/Pages/Story/Controller/CourseStoryViewController.swift
+++ Pubery/Features/Course/Pages/Story/Controller/CourseStoryViewController.swift
@@ -16,10 +16,66 @@ class CourseStoryViewController: UIViewController, UIScrollViewDelegate {
 	
 	let pages:[[Any]] =
 		[
-			[StoryStyle1(), "page1", "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang."],
-			[StoryStyle1(), "page2a", "Putri merasakan perutnya seperti ditekan-tekan."],
+//			[
+//				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 50, left: 10, bottom: 250, right: 10), size: .zero),
+////				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+////				StoryLabelRegular(text: "asdasd.", padding: UIEdgeInsets(top: 0, left: 20, bottom: 90, right: 20), size: .zero)
+//			],
+//			[
+//				StoryImage(imageName: "page2a", padding:  UIEdgeInsets(top: 50, left: 10, bottom: 250, right: 10), size: .zero),
+////				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Putri merasakan perutnya seperti ditekan-tekan. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+//				StoryLabelRegular(text: "Hmm, sepertinya Putri tahu apa yang terjadi pada dirinya, namun dia bingung apa yang harus dilakukan untuk pertama kali!", padding: UIEdgeInsets(top: 10, left: 20, bottom: 0, right: 20), size: .zero)
+//			],
+//			[
+//				StoryLabelRegular(text: "Putri merasakan perutnya seperti ditekan-tekan. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 150, left: 20, bottom: 0, right: 20), size: .zero),
+//				StoryImage(imageName: "page3", padding:  UIEdgeInsets(top: 10, left: 10, bottom: 200, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Hmm, sepertinya Putri tahu apa yang terjadi pada dirinya, namun dia bingung apa yang harus dilakukan untuk pertama kali!", padding: UIEdgeInsets(top: 10, left: 20, bottom: 0, right: 20), size: .zero)
+//			],
+//			[
+//				StoryLabelRegular(text: "Putri merasakan perutnya seperti ditekan-tekan. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+//			],
+//			[
+//				StoryLabelRegular(text: "Putri merasakan perutnya seperti ditekan-tekan. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+//			],
+//			[
+//				StoryLabelRegular(text: "Putri merasakan perutnya seperti ditekan-tekan. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+//			],
+//			[
+//				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 50, left: 10, bottom: 250, right: 10), size: .zero),
+////				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+////				StoryLabelRegular(text: "asdasd.", padding: UIEdgeInsets(top: 0, left: 20, bottom: 90, right: 20), size: .zero)
+//			],
+//			[
+//				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 50, left: 10, bottom: 250, right: 10), size: .zero),
+////				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+////				StoryLabelRegular(text: "asdasd.", padding: UIEdgeInsets(top: 0, left: 20, bottom: 90, right: 20), size: .zero)
+//			],
+//			[
+//				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 50, left: 10, bottom: 250, right: 10), size: .zero),
+////				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+////				StoryLabelRegular(text: "asdasd.", padding: UIEdgeInsets(top: 0, left: 20, bottom: 90, right: 20), size: .zero)
+//			],
+//			[
+//				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 50, left: 10, bottom: 250, right: 10), size: .zero),
+//				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+//			],
+			[
+				
+				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 40, left: 20, bottom: 0, right: 20), size: .zero),
+				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+				StoryImage(imageName: "page1", padding:  UIEdgeInsets(top: 20, left: 10, bottom: 0, right: 10), size: CGSize(width: 0, height: 220)),
+				StoryLabelRegular(text: "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang.", padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20), size: .zero),
+			],
+//			[StoryStyle1(), "page1", "Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang."],
+//			[StoryStyle1(), "page2a", "Putri merasakan perutnya seperti ditekan-tekan."],
 //			[ImageStatic(), "page2b", "Hmm, sepertinya Putri tahu apa yang terjadi pada dirinya, namun dia bingung apa yang harus dilakukan untuk pertama kali!"],
-			[StoryStyle2(), "page3", "Ketika Putri membuka celana dalamnya di kamar mandi, Putri kaget karena menemukan bercak-bercak merah.","Hmm, sepertinya Putri tahu apa yang terjadi pada dirinya, namun dia bingung apa yang harus dilakukan untuk pertama kali!"],
+//			[StoryStyle2(), "page3", "Ketika Putri membuka celana dalamnya di kamar mandi, Putri kaget karena menemukan bercak-bercak merah.","Hmm, sepertinya Putri tahu apa yang terjadi pada dirinya, namun dia bingung apa yang harus dilakukan untuk pertama kali!"],
 //			[ImageStatic.instantiate(imageName: "page1", label: "Halo")]
 //			[ImageStatic(), "ada haptic", UIImage(named: "page1")!],
 //			[UIImage(named: "page1")!,"Suatu pagi Putri merasakan sakit perut. Namun, tidak seperti biasanya, sakit perut Putri kali ini tidak kunjung menghilang."],
@@ -34,20 +90,26 @@ class CourseStoryViewController: UIViewController, UIScrollViewDelegate {
 	var contentWidth: CGFloat = 0.0
 	var contentHeight: CGFloat = 0.0
 	var contentFrame = CGRect(x: 0, y: 0, width: 0, height: 0)
+	var set = false
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		self.view.layoutIfNeeded()
-		
+	}
+	
+	override func viewDidLayoutSubviews() {
+		// storyboard autolayout need to be updated first from here
+		// this function will trigger everytime the page is changed
+		// so set an if statement to prevent repeated triggers as workaround
+		if (set) { return }
+		set = true
 		setupScrollView()
 		setupContent()
 		setupPageControl()
 		updatePageControl()
 		setupButton()
-		self.scrollView.backgroundColor = .gray.withAlphaComponent(0.2)
 	}
 	
-	func setupScrollView(){
+	func setupScrollView() {
 		scrollView.delegate = self
 		scrollView.contentSize.width = scrollView.frame.size.width * CGFloat(pages.count)
 		scrollView.contentSize.height = 1
@@ -56,110 +118,89 @@ class CourseStoryViewController: UIViewController, UIScrollViewDelegate {
 		scrollView.showsHorizontalScrollIndicator = false
 	}
 	
-	func setupContent(){
+	func setupContent() {
 		contentWidth = scrollView.frame.size.width
 		contentHeight = scrollView.frame.size.height
+		
 		contentFrame.size = CGSize(width: contentWidth, height: contentHeight)
 		
 		for pageIndex in 0..<pages.count {
 			contentFrame.origin.x = contentWidth * CGFloat(pageIndex)
 			
-			let view = UIView(frame: contentFrame)
-			view.backgroundColor = .red.withAlphaComponent(0.1)
+			let contentView = UIView(frame: contentFrame)
 			
+			let elementsContainer = UIView()
+
+			let backgroundImage = UIImageView()
+			backgroundImage.image = UIImage(named: "background")
+			backgroundImage.contentMode = .scaleToFill
 			
-			if (pages[pageIndex][0] is StoryStyle1){
-				let v = StoryStyle1()
-				v.imageView.image = UIImage(named: pages[pageIndex][1] as! String)
-				v.label.text = pages[pageIndex][2] as? String
-//				v.label.sizeToFit()
-				view.addSubview(v)
-				v.translatesAutoresizingMaskIntoConstraints = false
-				NSLayoutConstraint.activate([
-					v.topAnchor.constraint(equalTo: view.topAnchor,constant: 0),
-					v.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -50),
-
-					v.widthAnchor.constraint(equalTo: view.widthAnchor),
-					v.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-				])
-			} else if (pages[pageIndex][0] is StoryStyle2){
-				let v = StoryStyle2()
-				v.imageView.image = UIImage(named: pages[pageIndex][1] as! String)
-				v.label.text = pages[pageIndex][2] as? String
-				v.label2.text = pages[pageIndex][3] as? String
-//				v.label.sizeToFit()
-				view.addSubview(v)
-				v.translatesAutoresizingMaskIntoConstraints = false
-				NSLayoutConstraint.activate([
-					v.topAnchor.constraint(equalTo: view.topAnchor,constant: 0),
-					v.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -50),
-
-					v.widthAnchor.constraint(equalTo: view.widthAnchor),
-					v.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
-				])
-			}
+			contentView.addSubview(backgroundImage)
+			backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+			backgroundImage.setConstraint(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor)
+		
+			var elements: [UIView] = []
+			let lastElementIndex = pages[pageIndex].count-1
 			
 			for elementIndex in 0..<pages[pageIndex].count {
 				
 				
-//				view.addSubview(pages[pageIndex][elementIndex] as! UIView)
-				
-				
-//				if (pages[pageIndex][elementIndex] is UIImage){
-////					let imageView = UIImageView(frame: CGRect(x: 0, y: 80 * (elementIndex+1), width: 200, height: 200))
-////					imageView.image = pages[pageIndex][elementIndex] as? UIImage
-////					imageView.contentMode = .scaleAspectFit
-//					let imageView = ImageStatic()
-//					imageView.translatesAutoresizingMaskIntoConstraints = false
-//					imageView.imageView.image = UIImage(named: "page3")
-//
-//
-//					view.addSubview(imageView)
-//					NSLayoutConstraint.activate([
-//						imageView.topAnchor.constraint(equalTo: view.topAnchor,constant: 50),
-//						imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: 50),
-////						imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 50),
-////						imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: 50),
-////						imageView.widthAnchor.constraint(equalTo: view.widthAnchor),
-//						imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-////						imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-//
-////								imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//		//					  imageView.leadingAnchor.constraint(
-//		//						equalTo: self.leadingAnchor,
-//		//						constant: 50),
-//		//					  imageView.trailingAnchor.constraint(
-//		//						equalTo: self.trailingAnchor,
-//		//						constant: -padding)
-//					])
-//				} else if (pages[pageIndex][elementIndex] is String) {
-////					let label = UILabel(frame: CGRect(x: 0, y: 120 + 80 * (elementIndex+1), width: 300, height: 120))
-////					label.text = pages[pageIndex][elementIndex] as? String
-////					label.numberOfLines = 0
-////					label.lineBreakMode = .byWordWrapping
-//////						l.textAlignment = .center
-////					label.sizeToFit()
-////					view.addSubview(label)
-////					NSLayoutConstraint.activate([
-////						label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
-////		//						imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-////		//					  imageView.leadingAnchor.constraint(
-////		//						equalTo: self.leadingAnchor,
-////		//						constant: 50),
-////		//					  imageView.trailingAnchor.constraint(
-////		//						equalTo: self.trailingAnchor,
-////		//						constant: -padding)
-////					])
-//				}
+				if (pages[pageIndex][elementIndex] is StoryImage) {
+					
+					let storyImage = pages[pageIndex][elementIndex] as! StoryImage
+					
+					let imageView = UIImageView()
+					imageView.backgroundColor = .red
+					imageView.image = UIImage(named: storyImage.imageName)
+					imageView.contentMode = .scaleAspectFit
+					
+					elementsContainer.addSubview(imageView)
+					
+					imageView.translatesAutoresizingMaskIntoConstraints = false
+					
+					if (elementIndex == 0) {
+						imageView.setConstraint(top: elementsContainer.topAnchor, leading: elementsContainer.leadingAnchor, bottom: elementsContainer.bottomAnchor, trailing: elementsContainer.trailingAnchor, padding: storyImage.padding, size: storyImage.size)
+					} else {
+						let previousElement = elements[elementIndex-1]
+						imageView.setConstraint(top: previousElement.bottomAnchor, leading: elementsContainer.leadingAnchor, bottom: elementIndex == lastElementIndex ? elementsContainer.bottomAnchor : nil, trailing: elementsContainer.trailingAnchor, padding: storyImage.padding, size: storyImage.size)
+					}
+					
+					elements.append(imageView)
+					
+				} else if (pages[pageIndex][elementIndex] is StoryLabelRegular) {
+					
+					let storyLabel = pages[pageIndex][elementIndex] as! StoryLabelRegular
+					
+					let label = UILabel()
+					label.text = storyLabel.text
+					label.textAlignment = .justified
+					label.numberOfLines = 0
+					label.lineBreakMode = .byWordWrapping
+					label.sizeToFit()
+					
+					elementsContainer.addSubview(label)
+					label.translatesAutoresizingMaskIntoConstraints = false
+					
+					if (elementIndex == 0 && lastElementIndex == 0) {
+						label.setConstraint(top: elementsContainer.topAnchor, leading: elementsContainer.leadingAnchor, bottom: elementsContainer.bottomAnchor, trailing: elementsContainer.trailingAnchor, padding: storyLabel.padding, size: storyLabel.size)
+					} else if (elementIndex == 0) {
+						label.setConstraint(top: elementsContainer.topAnchor, leading: elementsContainer.leadingAnchor, bottom: nil, trailing: elementsContainer.trailingAnchor, padding: storyLabel.padding, size: storyLabel.size)
+					} else {
+						let previousElement = elements[elementIndex-1]
+						label.setConstraint(top: previousElement.bottomAnchor, leading: elementsContainer.leadingAnchor, bottom: nil, trailing: elementsContainer.trailingAnchor, padding: storyLabel.padding, size: storyLabel.size)
+					}
+					
+					elements.append(label)
+				}
+				contentView.addSubview(elementsContainer)
 			}
-			scrollView.addSubview(view)
-
+			elementsContainer.translatesAutoresizingMaskIntoConstraints = false
+			elementsContainer.setConstraint(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: UIEdgeInsets(top: 0, left: 0, bottom: 50, right: 0))
+			scrollView.addSubview(contentView)
 		}
 	}
 	
-	func setupPageControl(){
+	func setupPageControl() {
 		pageControl.numberOfPages = pages.count
 		pageControl.currentPage = 0
 	}
@@ -184,13 +225,13 @@ class CourseStoryViewController: UIViewController, UIScrollViewDelegate {
 		pageNumber.sizeToFit()
 	}
 	
-	func setupButton(){
+	func setupButton() {
 		nextButton.layer.cornerRadius = 14
 	}
 
 	@IBAction func nextPage(_ sender: Any) {
 		let currentPage = getCurrentPage()
-		if (currentPage != pages.count-1){
+		if (currentPage != pages.count-1) {
 			scrollView.scrollRectToVisible(CGRect(x: contentWidth * CGFloat(currentPage+1), y: 0, width: contentWidth, height: 1), animated: true)
 		} else {
 
@@ -200,8 +241,76 @@ class CourseStoryViewController: UIViewController, UIScrollViewDelegate {
 
 	@IBAction func previousPage(_ sender: Any) {
 		let currentPage = getCurrentPage()
-		if (currentPage != 0){
+		if (currentPage != 0) {
 			scrollView.scrollRectToVisible(CGRect(x: contentWidth * CGFloat(currentPage-1), y: 0, width: contentWidth, height: 1), animated: true)
 		}
+	}
+}
+
+extension UIView {
+	
+	func fillSuperview() {
+		setConstraint(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+	}
+	
+	func constraintSize(to view: UIView) {
+		widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+		heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+	}
+	
+	func setConstraint(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+		translatesAutoresizingMaskIntoConstraints = false
+		
+		if let top = top {
+			topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+		}
+		
+		if let leading = leading {
+			leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+		}
+		
+		if let bottom = bottom {
+			bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+		}
+		
+		if let trailing = trailing {
+			trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+		}
+		
+		if size.width != 0 {
+			widthAnchor.constraint(equalToConstant: size.width).isActive = true
+		}
+		
+		if size.height != 0 {
+			heightAnchor.constraint(equalToConstant: size.height).isActive = true
+		}
+	}
+}
+
+class CustomConstraint {
+	var padding: UIEdgeInsets
+	var size: CGSize
+	
+	init(padding: UIEdgeInsets, size: CGSize) {
+		self.padding = padding
+		self.size = size
+	}
+}
+
+class StoryImage: CustomConstraint {
+	var imageName: String
+	
+	init(imageName: String, padding: UIEdgeInsets, size: CGSize) {
+		self.imageName = imageName
+		super.init(padding: padding, size: size)
+	}
+}
+
+class StoryLabelRegular: CustomConstraint {
+	var text: String
+	
+	init(text: String, padding: UIEdgeInsets, size: CGSize) {
+		self.text = text
+		super.init(padding: padding, size: size)
 	}
 }
---
+++ Pubery/Features/Course/Pages/Story/View/ImageA.swift
@@ -0,0 +1,20 @@
+//
+//  ImageA.swift
+//  Pubery
+//
+//  Created by Antony Susanto on 30/07/21.
+//
+
+import UIKit
+
+class ImageA: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
