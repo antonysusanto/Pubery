edba548591334c9af236e661eb1d4b910c30a0b8
olip <olivia.susanti@binus.ac.id>
2021-08-05 21:16:23 +0700

    Fix Bug Mini Game 1 - Cleanning

--- Pubery/Features/Games/Brushing/Controller/BrushingViewController.swift
+++ Pubery/Features/Games/Brushing/Controller/BrushingViewController.swift
@@ -19,9 +19,9 @@ class BrushingViewController: UIViewController {
     
     let customAlert = CustomAlert()
     var movedViewOrigin: CGPoint!
-    var loading_1: UIImage!
-    var loading_2: UIImage!
-    var loading_3: UIImage!
+    var bubble_1: UIImage!
+    var bubble_2: UIImage!
+    var bubble_3: UIImage!
     var images: [UIImage]!
     var animatedImage: UIImage!
     var counterIntersect = 0
@@ -32,20 +32,19 @@ class BrushingViewController: UIViewController {
         addPanGesture(view: movedImage)
         movedViewOrigin = movedImage.frame.origin
         view.bringSubviewToFront(movedImage)
-        
+//        targetImage.backgroundColor = .yellow
         bubleImage.isHidden = true
         endButton.isHidden = true
         
         customAlert.showAlert(with: "Tekan dan tahan sikat, lalu arahkan ke  noda sampai bersih.", on: self)
         
-        loading_1 = UIImage(named: "bubble1")
-        loading_2 = UIImage(named: "bubble2")
-        loading_3 = UIImage(named: "bubble3")
-        images = [loading_1, loading_2, loading_3]
+        bubble_1 = UIImage(named: "bubble1")
+        bubble_2 = UIImage(named: "bubble2")
+        bubble_3 = UIImage(named: "bubble3")
+        images = [bubble_1, bubble_2, bubble_3]
     }
     func addPanGesture(view: UIView){
         let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan(sender:)))
-        pan.maximumNumberOfTouches = 4
         view.addGestureRecognizer(pan)
     }
     
@@ -73,43 +72,57 @@ class BrushingViewController: UIViewController {
     // refactor
     @objc func handlePan(sender: UIPanGestureRecognizer) {
         let movedView = sender.view!
+//        movedView.backgroundColor = .yellow
         switch sender.state {
         case .began:
             moveViewWithPan(view: movedView, sender: sender)
-            if counterIntersect < 1 {
-                targetImage.alpha = 0.9
-                startAnimation()
-                bubleImage.isHidden = false
-            }
-            else if counterIntersect < 2 {
-                targetImage.alpha = 0.7
-                startAnimation()
-                bubleImage.isHidden = false
-            }
-            else if counterIntersect < 3{
-                targetImage.alpha = 0.5
-                startAnimation()
-                bubleImage.isHidden = false
-            }
-            else if counterIntersect < 4 {
-                targetImage.alpha = 0.1
-                startAnimation()
-                bubleImage.isHidden = false
-            }
         case .changed:
             moveViewWithPan(view: movedView, sender: sender)
+            if movedView.frame.intersects(targetImage.frame) {
+                counterIntersect += 1
+                bubleImage.isHidden = false
+            }
+            else if movedView.frame.intersects(backgroundImage.frame){
+                bubleImage.isHidden = true
+            }
+//            print(counterIntersect)
+            if counterIntersect < 50 {
+                targetImage.alpha = 0.9
+//                startAnimation()
+//                bubleImage.isHidden = false
+//                bubleImage.image = bubble_1
+                bubleImage.alpha = 0.9
+            }
+            else if counterIntersect < 100 {
+                targetImage.alpha = 0.5
+//                startAnimation()
+//                bubleImage.isHidden = false
+//                bubleImage.image = bubble_2
+                bubleImage.alpha = 0.5
+            }
+            else if counterIntersect < 150 {
+                targetImage.alpha = 0.3
+//                startAnimation()
+//                bubleImage.isHidden = false
+//                bubleImage.image = bubble_3
+                bubleImage.alpha = 0.3
+            }
+            else if counterIntersect < 200 {
+                targetImage.alpha = 0.1
+//                startAnimation()
+                bubleImage.isHidden = true
+            }
         case .ended:
             if movedView.frame.intersects(targetImage.frame) {
                 counterIntersect += 1
-                if counterIntersect > 3 {
+                if counterIntersect > 200 {
                     endView(view: movedView)
                 }
-                stopAnimation()
+//                stopAnimation()
             }
         default:
             break
         }
-        print(sender.state)
     }
     
     func moveViewWithPan(view: UIView, sender: UIPanGestureRecognizer) {
@@ -121,7 +134,8 @@ class BrushingViewController: UIViewController {
     func endView(view: UIView) {
 //        UIView.animate(withDuration: 0.6, animations: {view.alpha = 1
 //        })
-        stopAnimation()
+//        stopAnimation()
+        bubleImage.isHidden = true
         targetImage.isHidden = true
         closeButton.isHidden = true
         endButton.isHidden = false
