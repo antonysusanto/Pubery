af7dc3e62c11fb1def7458f1616a13e94ee47877
antony-hc <antony@hepicircle.com>
2021-08-24 11:38:51 +0700

    breathing game

--- Pubery/Features/Games/Breathing/Controller/BreathingViewController.swift
+++ Pubery/Features/Games/Breathing/Controller/BreathingViewController.swift
@@ -9,14 +9,56 @@ import UIKit
 
 class BreathingViewController: UIViewController {
 
+	@IBOutlet weak var lightCircle: UIImageView!
+	@IBOutlet weak var darkCircle: UIImageView!
+	@IBOutlet weak var label: UILabel!
+	@IBOutlet weak var button: UIButton!
+	
 	let customAlert = CustomAlert()
+	var count = 0
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		customAlert.showAlert(with: "Coming soon", on: self)
+//		customAlert.showAlert(with: "Petunjuk", on: self)
+		self.label.text = "Ketika merasa ingin marah, coba untuk tetap tenang dan atur napas pelan-pelan"
+		DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+			self.animateCircle()
+		}
 	}
-
+	
+	func animateCircle(){
+		if (count == 5) {
+			finish()
+			return
+		}
+		count += 1
+		DispatchQueue.main.async {
+			UIView.animate(withDuration: 3, animations: {
+				self.label.text = "Tarik napas..."
+				self.lightCircle.transform = CGAffineTransform(scaleX: 2, y: 2)
+			}) { _ in
+				UIView.animate(withDuration: 3, animations: {
+					self.label.text = "Hembuskan napas..."
+					self.lightCircle.transform = CGAffineTransform(scaleX: 1, y: 1)
+				}) { _ in
+					self.animateCircle()
+				}
+			}
+		}
+	}
+	
+	func finish() {
+		DispatchQueue.main.async {
+			self.label.text = "Kerja bagus, Kamu telah berhasil mengontrol emosimu!"
+			self.button.isHidden = false
+		}
+	}
+	
+	@IBAction func close(_ sender: Any) {
+		self.navigationController?.popViewController(animated: false)
+	}
+	
 	@IBAction func back(_ sender: Any) {
 		self.navigationController?.popViewController(animated: false)
 	}
