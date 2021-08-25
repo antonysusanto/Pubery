5c7cc28db437a958f86bc69edfc7d5695d859535
antony-hc <antony@hepicircle.com>
2021-08-25 22:19:16 +0700

    play sound when minigames end

---
+++ Pubery/Helper/PlaySoundAsset.swift
@@ -0,0 +1,24 @@
+//
+//  PlaySoundAsset.swift
+//  Pubery
+//
+//  Created by Antony Susanto on 25/08/21.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundAsset {
+	static var player: AVAudioPlayer?
+	
+	static func play(_ assetName : String) {
+		guard let audioData = NSDataAsset(name: assetName)?.data else { return }
+
+		do {
+		  player = try AVAudioPlayer(data: audioData)
+		  player?.play()
+		} catch {
+		  print(error.localizedDescription)
+	  }
+  }
+}
