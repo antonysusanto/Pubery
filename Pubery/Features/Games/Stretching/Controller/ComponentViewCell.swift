243e660ff138228aa877fa5fa75eb31e317e003e
Carlos Gamaliel Manurung <carlosgamaliel>
2021-08-27 11:08:28 +0700

    Create mini games course 8

---
+++ Pubery/Features/Games/Stretching/Controller/ComponentViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ComponentViewCell.swift
+//  Pubery
+//
+//  Created by Carlos Gamaliel Manurung on 27/08/21.
+//
+
+import UIKit
+
+class ComponentViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImage: UIImageView!
+    
+}
